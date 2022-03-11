b8989451e38408e997e4dee8876144afb0fb22f8
DanielMoreiraCoelho <93950874+DanielMoreiraCoelho@users.noreply.github.com>
2022-03-11 17:38:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,11 +6,11 @@ var music;
 function preload(){
     // carregue o som aqui
 
-    music = preload
+    music = loadSound("music.mp3")
 }
 
 function setup(){
-    tela = createTela(800,600);
+    tela = createCanvas(800,600);
 
     bloco1 = createSprite(0,580,360,30);
     bloco1.shapeColor = "blue";
@@ -18,21 +18,21 @@ function setup(){
     bloco2 = createSprite(295,580,200,30);
     bloco2.shapeColor = "orange";
 
-    bloco3 = createSprite(590,580,200,30);
+    bloco3 = createSprite(515,580,200,30);
     bloco3.shapeColor = "red";
 
-    bloco4 = createSprite(800,580,200,30);
+    bloco4 = createSprite(740,580,220,30);
     bloco4.shapeColor = "green";
 
     bola = createSprite(random(20,750),100, 40,40);
     bola.shapeColor = rgb(255,255,255);
-    bola.velocityX = 1.5;
-    bola.velocityY = -3;
+    bola.velocityX = -3;
+    bola.velocityY = -10;
 }
 
 function draw() {
     background(rgb(169,169,169));
-    arestas=createArestasprites();
+    arestas = createEdgeSprites();
     bola.bounceOff(arestas);
 
     
@@ -40,7 +40,7 @@ function draw() {
     if(bloco1.isTouching(bola) && bola.bounceOff(bloco1)){
         bola.shapeColor = "blue";
         music.play();
-        bola.bounceOff(boloco1);
+        
     }
 
 
@@ -48,18 +48,14 @@ function draw() {
         bola.shapeColor = "orange";
         bola.velocityX = 0;
         bola.velocityY = 0;
-        music.play(false);
+        music.stop();
     }
 
     if(bloco3.isTouching(bola) && bola.bounceOff(bloco3)){
-        bola.shapeColor = "green";
-        music.play();
-        bola.bounceOff(boloco3);
+        bola.shapeColor = "red";
     }  
         if(bloco4.isTouching(bola) && bola.bounceOff(bloco4)){
-            bola.shapeColor = "red";
-            music.play();
-            bola.bounceOff(boloco4);
+            bola.shapeColor = "green";
         }
             drawSprites();
 }
